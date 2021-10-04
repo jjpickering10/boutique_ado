272b2171e671cff9427540c2e44324a43dbfa938
jjpickering10 <jjpickering10@live.com>
2021-10-04 15:07:57 +0000

    Add checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,40 +53,75 @@ form.addEventListener('submit', function(ev) {
   $('#payment-form').fadeToggle(100)
   $('#loading-overlay').fadeToggle(100)
 
-  // If the client secret was rendered server-side as a data-secret attribute
-  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-    }
-  }).then(function(result) {
-    if (result.error) {
-      // Show error to your customer (e.g., insufficient funds)
-      var errorDiv = document.getElementById('card-errors')
-      var html = `
-            <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-            </span>
-            <span>${result.error.message}</span>
-        `
-        $(errorDiv).html(html)
-        $('#payment-form').fadeToggle(100)
-        $('#loading-overlay').fadeToggle(100)
+  var saveInfo = Boolean($('#id-save-info').attr('checked'))
+  var csrfToken = $('input[name=csrfmiddlewaretoken]').val()
+  var postData = {
+    'csrfmiddlewaretoken': csrfToken,
+    'client_secret': clientSecret,
+    'save_info': saveInfo,
+  }
+  var url = '/checkout/cache_checkout_data/'
 
-        card.update({
-            'disabled': false
-        })
-        $('#submit-button').attr('disabled', false)
-    } else {
-      // The payment has been processed!
-      if (result.paymentIntent.status === 'succeeded') {
-        // Show a success message to your customer
-        // There's a risk of the customer closing the window before callback
-        // execution. Set up a webhook or plugin to listen for the
-        // payment_intent.succeeded event that handles any business critical
-        // post-payment actions.
-        form.submit()
+  $.post(url, postData).done(function() {
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: $.trim(form.full_name.value),
+          phone: $.trim(form.phone_number.value),
+          email: $.trim(form.email.value),
+          address: {
+            line1: $.trim(form.street_address1.value),
+            line2: $.trim(form.street_address2.value),
+            city: $.trim(form.town_or_city.value),
+            country: $.trim(form.country.value),
+            state: $.trim(form.county.value),
+          }
+        }
+      },
+      shipping: {
+        name: $.trim(form.full_name.value),
+        phone: $.trim(form.phone_number.value),
+        address: {
+          line1: $.trim(form.street_address1.value),
+          line2: $.trim(form.street_address2.value),
+          city: $.trim(form.town_or_city.value),
+          country: $.trim(form.country.value),
+          postal_code: $.trim(form.postcode.value),
+          state: $.trim(form.county.value),
+        }
       }
-    }
-  });
+    }).then(function(result) {
+      if (result.error) {
+        // Show error to your customer (e.g., insufficient funds)
+        var errorDiv = document.getElementById('card-errors')
+        var html = `
+              <span class="icon" role="alert">
+                  <i class="fas fa-times"></i>
+              </span>
+              <span>${result.error.message}</span>
+          `
+          $(errorDiv).html(html)
+          $('#payment-form').fadeToggle(100)
+          $('#loading-overlay').fadeToggle(100)
+  
+          card.update({
+              'disabled': false
+          })
+          $('#submit-button').attr('disabled', false)
+      } else {
+        // The payment has been processed!
+        if (result.paymentIntent.status === 'succeeded') {
+          // Show a success message to your customer
+          // There's a risk of the customer closing the window before callback
+          // execution. Set up a webhook or plugin to listen for the
+          // payment_intent.succeeded event that handles any business critical
+          // post-payment actions.
+          form.submit()
+        }
+      }
+    });
+  }).fail(function() {
+      location.reload()
+  })
 });
